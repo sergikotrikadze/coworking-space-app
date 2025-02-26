dbb736ef36e61851df52b2f57ed7f1ea633cecda
sergi <sergikotrikadze1@gmail.com>
2025-02-27 03:46:36 +0400

    Task 1 done

--- src/coworking/space/menus/AccountCreationMenu.java
+++ src/coworking/space/menus/AccountCreationMenu.java
@@ -6,26 +6,26 @@ import java.util.Scanner;
 
 public class AccountCreationMenu {
 
-    private static final String ADMIN_KEY = "admin123"; //if user enters this key they will be able to create an admin acc
+    private static final String ADMIN_KEY = "admin123"; // If user enters this key, they can create an admin account
 
     public void displayExplanationTxt() {
         System.out.println("------------------");
-        System.out.println("Please fill out all forms except last one if you are just a customer");
+        System.out.println("Please fill out the forms. Leave the admin key empty if you are a customer.");
         System.out.println("------------------");
     }
 
     public String getUserLogin(Scanner scanner) {
-        System.out.print("Enter your Login: ");
+        System.out.print("Enter your login: ");
         return scanner.nextLine();
     }
 
     public String getUserPassword(Scanner scanner) {
-        System.out.print("Enter your Password: ");
+        System.out.print("Enter your password: ");
         return scanner.nextLine();
     }
 
     public UserRole getUserRole(Scanner scanner) {
-        System.out.println("Enter admin KEY: ");
+        System.out.print("Enter admin KEY (or press Enter to continue as Customer): ");
         String input = scanner.nextLine();
         if (input.isEmpty()) return UserRole.CUSTOMER;
         if (input.equals(ADMIN_KEY)) return UserRole.ADMIN;
--- src/coworking/space/repository/UserRepository.java
+++ src/coworking/space/repository/UserRepository.java
@@ -5,14 +5,21 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class UserRepository {
-    
+
     private Set<User> users;
 
     public UserRepository() {
         this.users = new HashSet<>();
     }
-    
+
     public void addUser(User user) {
         users.add(user);
+    }
+
+    public User getUserByLogin(String login) {
+        return users.stream()
+                .filter(u -> u.getLogin().equals(login))
+                .findFirst()
+                .orElse(null);
     }
 }
